4514704d5ca9dcd6a66a4850374615c32718d543
Harish Duwadi <hduwadi@ucsd.edu>
2019-06-11 10:28:09 -0700

    Added local changes to remote -- project not longer active

---
+++ certbotmanager/certbot.go
@@ -0,0 +1,323 @@
+package certbotmanager
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"github.com/harishduwadi/tlsConnection/db"
+
+	"github.com/harishduwadi/sfcertbot/certbot"
+	"github.com/harishduwadi/sfcertbot/certificate"
+	certbotconfig "github.com/harishduwadi/sfcertbot/config"
+	"github.com/harishduwadi/tlsConnection/config"
+)
+
+// time out for setting up a connection
+const timeOut = 10
+
+func Start(db *db.Dbmanager, certConfig *certbotconfig.Configuration, opt *config.Opts) {
+	switch {
+	case opt.CertbotController:
+		certbotController(db, certConfig)
+	case opt.PopulateDB:
+		populateDB(db, certConfig)
+	default:
+	}
+}
+
+func certbotController(db *db.Dbmanager, certConfig *certbotconfig.Configuration) {
+
+	requestCertConfig := new(certbotconfig.Configuration)
+	requestCertConfig.KeyFileName = certConfig.KeyFileName
+	requestCertConfig.LetsEncryptUrl = certConfig.LetsEncryptUrl
+
+	// Get all the certificates that will expire in the next 7 days time in DB
+	expiredDomains, err := db.GetAllExpiredCommonNames(time.Now().In(time.UTC).AddDate(0, 0, 7))
+	if err != nil {
+		log.Println(config.Error, err)
+		return
+	}
+
+	// Get the certificate configuration for the expired domains
+	for _, cert := range certConfig.Certificates {
+		if _, ok := expiredDomains[cert.CommonName]; ok {
+			requestCertConfig.Certificates = append(requestCertConfig.Certificates, cert)
+		}
+	}
+
+	fmt.Println(requestCertConfig)
+
+	// Create a RSA key for the client (works fine)
+	certbot.Register(certConfig)
+
+	certpackage := new(certificate.Certificate)
+	acmeClient := certbot.CreateClient(requestCertConfig)
+
+	// Create new certificate for all the expired domains
+	for _, cert := range requestCertConfig.Certificates {
+		certpackage.CertConfig = &cert
+
+		certpackage.SetEnvironmentVariables()
+
+		rawcert, err := certpackage.CreateValidCertificate(acmeClient)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		certpackage.CleanEnvironmentVariables()
+
+		err = updateCertificate(db, rawcert)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = addToUpdateListTable(db, cert)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+}
+
+func populateDB(db *db.Dbmanager, certConfig *certbotconfig.Configuration) {
+
+	conf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	// Go through each certificate listed in the xml
+	for _, xmlCert := range certConfig.Certificates {
+
+		// Get IP for the domain name
+		ips := xmlCert.Endpoints
+
+		if len(ips) == 0 {
+			log.Fatalln(xmlCert.CommonName, "doesn't have any ip address")
+		}
+
+		// Get the certificate currently used by the domain;
+		// TODO: Here if there is no certificate, should return error.
+		// Instead of just logging the error, might need to create a new entry with time that is already expired
+		certificate, err := setUpConnection(conf, ips[0], "443", xmlCert.CommonName)
+		if err != nil {
+			log.Printf("Connection error with %s: %v\n", xmlCert.CommonName, err)
+			continue
+		}
+
+		// Change the SAN entries to the one listed in the xml file
+		// TODO: Removing from DB is not properly set yet
+		certificate.AlterDomains = xmlCert.SubjectAlternativeNames
+
+		log.Println("Adding Entry to DB wih commonname:", certificate.Subject, "and config name", xmlCert.CommonName)
+
+		// Add/Retrieve Key Table entry
+		keyid, err := updateKeyTable(db, xmlCert.Credential)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Add entry to Certificate Table
+		certificateID, err := updateCertificateTable(db, certificate, keyid)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// Add entry to the SANEntries table
+		err = updateSANTable(db, xmlCert.SubjectAlternativeNames, certificateID)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Add entries to Endpoint table
+		for _, ip := range ips {
+
+			endpointID, err := updateEndPointTable(db, ip)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			// Check if there already exists a relation
+			certendid, err := db.GetCertificateEndPointEntry(certificateID, endpointID)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			if certendid != -1 {
+				continue
+			}
+
+			// Add entry to certificateendpoint table
+			err = db.AddCertificateEndPointEntry(certificateID, endpointID)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func updateKeyTable(db *db.Dbmanager, credential certbotconfig.CredentialConfiguration) (int, error) {
+	id, err := db.GetKeyId(credential.APIKey, credential.APISecret)
+	if err != nil {
+		return id, err
+	}
+	if id == -1 {
+		err = db.AddKeyEntry(credential.APIKey, credential.APIKeyEnvironmentVariableName, credential.APISecret, credential.APISecretEnvironmentVariableName, credential.DNSProviderName)
+		if err != nil {
+			return -1, err
+		}
+		id, err = db.GetKeyId(credential.APIKey, credential.APISecret)
+		if err != nil {
+			return id, err
+		}
+	}
+	return id, nil
+
+}
+
+func updateCertificate(db *db.Dbmanager, rawCert []byte) error {
+	block, _ := pem.Decode(rawCert)
+	if block == nil {
+		log.Fatal("Failed to parse the certificate")
+	}
+	certificate, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return err
+	}
+
+	serialNumberInHex := fmt.Sprintf("%x", certificate.SerialNumber)
+	log.Println(certificate.Subject.CommonName, serialNumberInHex, certificate.NotBefore)
+	err = db.UpdateCertificateEntry(certificate.Subject.CommonName, serialNumberInHex, certificate.NotBefore, rawCert, certificate.Issuer.CommonName)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func addToUpdateListTable(db *db.Dbmanager, cert certbotconfig.CertificateConfiguration) error {
+
+	certID, err := db.GetCertificateEntry(cert.CommonName)
+	if err != nil || certID == -1 {
+		return err
+	}
+
+	for _, ip := range cert.Endpoints {
+		endpointID, err := db.GetEndPointEntry(ip)
+		if err != nil || endpointID == -1 {
+			return err
+		}
+
+		err = db.AddUpdateListEntry(certID, endpointID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func updateCertificateTable(db *db.Dbmanager, certificate *config.CertificateEntry, keyid int) (int, error) {
+	certificateID, err := db.GetCertificateEntry(certificate.Subject)
+	if err != nil {
+		return -1, err
+	}
+	if certificateID == -1 {
+		err = db.AddCertificateEntry(certificate.Subject, certificate.SerialNumber, certificate.Validity, nil, certificate.Issuer, keyid)
+		if err != nil {
+			return -1, err
+		}
+		certificateID, err = db.GetCertificateEntry(certificate.Subject)
+		if err != nil {
+			return -1, err
+		}
+	}
+	return certificateID, err
+}
+
+func updateSANTable(db *db.Dbmanager, sans []string, certificateID int) error {
+	for _, sanname := range sans {
+		sanID, err := db.GetSANEntry(sanname, certificateID)
+		if err != nil {
+			return err
+		}
+		if sanID != -1 {
+			continue
+		}
+		err = db.AddSANEntry(sanname, certificateID)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+	return nil
+}
+
+func updateEndPointTable(db *db.Dbmanager, ip string) (int, error) {
+	endpointID, err := db.GetEndPointEntry(ip)
+	if err != nil {
+		return -1, err
+	}
+	if endpointID != -1 {
+		return endpointID, nil
+	}
+	err = db.AddEndPointEntry(ip)
+	if err != nil {
+		return -1, err
+	}
+	endpointID, err = db.GetEndPointEntry(ip)
+	if err != nil {
+		return -1, err
+	}
+	return endpointID, nil
+}
+
+func setUpConnection(conf *tls.Config, ipaddr string, port string, serverName string) (*config.CertificateEntry, error) {
+	conf.ServerName = serverName
+
+	// Set up a connection with a time-out
+	conn, err := net.DialTimeout("tcp", ipaddr+":"+port, timeOut*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	tlsConn := tls.Client(conn, conf)
+	defer tlsConn.Close()
+
+	log.Println("Performing Handshake with host completed")
+
+	err = tlsConn.Handshake()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("HandShake with host completed")
+
+	cert := tlsConn.ConnectionState().PeerCertificates[0]
+
+	if cert == nil {
+		return nil, errors.New("No certificate for " + serverName + " with address " + ipaddr)
+	}
+
+	serialNumberInHex := fmt.Sprintf("%x", cert.SerialNumber)
+
+	certificate := &config.CertificateEntry{
+		Subject:      cert.Subject.CommonName,
+		SerialNumber: serialNumberInHex,
+		Validity:     cert.NotAfter.In(time.UTC),
+		AlterDomains: cert.DNSNames,
+		Issuer:       cert.Issuer.CommonName,
+	}
+
+	return certificate, nil
+}
--- db/db.go
+++ db/db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/harishduwadi/tlsConnection/config"
@@ -27,9 +28,9 @@ func New(config *config.DbConfig) (*Dbmanager, error) {
 	return dbm, nil
 }
 
-func (db *Dbmanager) AddCertificateEntry(servername string, ip string, commonname string, serialnumber string, validity time.Time, issuer string) error {
+func (db *Dbmanager) AddCertificateEntry(commonname string, serialnumber string, validity time.Time, rawCert []byte, issuer string, keyid int) error {
 	queryString := "Insert into certificates values(DEFAULT, $1, $2, $3, $4, $5, $6)"
-	_, err := db.dbsql.Exec(queryString, servername, ip, commonname, serialnumber, validity, issuer)
+	_, err := db.dbsql.Exec(queryString, commonname, serialnumber, validity, rawCert, issuer, keyid)
 	if err != nil {
 		return errors.New("AddCertificateEntry " + err.Error())
 	}
@@ -45,9 +46,36 @@ func (db *Dbmanager) AddSANEntry(sanname string, certificateID int) error {
 	return nil
 }
 
-func (db *Dbmanager) GetCertificateEntry(servername string, ip string) (int, error) {
-	queryString := "Select id From certificates Where servername=$1 and ip=$2"
-	result := db.dbsql.QueryRow(queryString, servername, ip)
+func (db *Dbmanager) AddEndPointEntry(ip string) error {
+	queryString := "Insert Into endpoints Values(DEFAULT, $1)"
+	_, err := db.dbsql.Exec(queryString, ip)
+	if err != nil {
+		return errors.New("AddEndPointEntry " + err.Error())
+	}
+	return nil
+}
+
+func (db *Dbmanager) AddCertificateEndPointEntry(certificateID int, endpointID int) error {
+	queryString := "Insert Into certificateendpoint values(DEFAULT, $1, $2)"
+	_, err := db.dbsql.Exec(queryString, certificateID, endpointID)
+	if err != nil {
+		return errors.New("AddCertificateEndPointEntry " + err.Error())
+	}
+	return nil
+}
+
+func (db *Dbmanager) AddUpdateListEntry(certificateID int, endpointID int) error {
+	queryString := "Insert Into updateList values(DEFAULT, $1, $2, DEFAULT)"
+	_, err := db.dbsql.Exec(queryString, certificateID, endpointID)
+	if err != nil {
+		return errors.New("AddUpdateListEntry " + err.Error())
+	}
+	return nil
+}
+
+func (db *Dbmanager) GetCertificateEntry(commonname string) (int, error) {
+	queryString := "Select id From certificates Where commonname=$1"
+	result := db.dbsql.QueryRow(queryString, commonname)
 	var certificateId int
 	err := result.Scan(&certificateId)
 	if err == sql.ErrNoRows {
@@ -59,10 +87,30 @@ func (db *Dbmanager) GetCertificateEntry(servername string, ip string) (int, err
 	return certificateId, nil
 }
 
-func (db *Dbmanager) GetExpireDate(servername string, ip string) (time.Time, error) {
-	queryString := "Select validity From certificates Where servername=$1 and ip=$2"
-	result := db.dbsql.QueryRow(queryString, servername, ip)
+func (db *Dbmanager) GetAllExpiredCommonNames(curTime time.Time) (map[string]string, error) {
+	expiredDomains := make(map[string]string)
+
+	queryString := "Select commonname From certificates Where validity<$1"
+	rows, err := db.dbsql.Query(queryString, curTime)
+	if err != nil {
+		return expiredDomains, errors.New("GetAllExpiredCommonNames " + err.Error())
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var domain string
+		err := rows.Scan(&domain)
+		if err != nil {
+			return expiredDomains, errors.New("GetAllExpiredCommonNames " + err.Error())
+		}
+		expiredDomains[domain] = ""
+	}
+	return expiredDomains, nil
+}
+
+func (db *Dbmanager) GetExpireDate(commonname string, ip string) (time.Time, error) {
 	var expireDate time.Time
+	queryString := "Select validity From certificates Where commonname=$1 and ip=$2"
+	result := db.dbsql.QueryRow(queryString, commonname, ip)
 	err := result.Scan(&expireDate)
 	if err == sql.ErrNoRows {
 		return time.Now(), nil
@@ -74,9 +122,9 @@ func (db *Dbmanager) GetExpireDate(servername string, ip string) (time.Time, err
 }
 
 func (db *Dbmanager) GetSANEntry(sanname string, certificateID int) (int, error) {
+	var sanID int
 	queryString := "Select id From sanentries where name=$1 and certificateID=$2"
 	result := db.dbsql.QueryRow(queryString, sanname, certificateID)
-	var sanID int
 	err := result.Scan(&sanID)
 	if err == sql.ErrNoRows {
 		return -1, nil
@@ -87,9 +135,98 @@ func (db *Dbmanager) GetSANEntry(sanname string, certificateID int) (int, error)
 	return sanID, nil
 }
 
-func (db *Dbmanager) UpdateCertificateEntry(id int, commonname string, serialnumber string, validity time.Time, issuer string) error {
-	queryString := "Update certificates set commonname=$1, serialnumber=$2, validity=$3, issuer=$4 where id=$5"
-	_, err := db.dbsql.Exec(queryString, commonname, serialnumber, validity, issuer, id)
+func (db *Dbmanager) GetEndPointEntry(ip string) (int, error) {
+	var ipaddrid int
+	queryString := "Select id From Endpoints Where ip=$1"
+	row := db.dbsql.QueryRow(queryString, ip)
+	err := row.Scan(&ipaddrid)
+	if err == sql.ErrNoRows {
+		return -1, nil
+	}
+	if err != nil {
+		return -1, errors.New("GetEndPointEntry " + err.Error())
+	}
+	return ipaddrid, nil
+}
+
+func (db *Dbmanager) GetCertificateEndPointEntry(certificateID int, endpointid int) (int, error) {
+	var id int
+	queryString := "Select id From CertificateEndPoint Where certificateId=$1 and endpointID=$2"
+	row := db.dbsql.QueryRow(queryString, certificateID, endpointid)
+	err := row.Scan(&id)
+	if err == sql.ErrNoRows {
+		return -1, nil
+	}
+	if err != nil {
+		return -1, errors.New("GetCertificateEndPointEntry " + err.Error())
+	}
+	return id, nil
+}
+func (db *Dbmanager) GetCertificateEntriesForEndPoint(endpoint string) ([]config.CertificateEntry, error) {
+	queryString := `Select c.commonname, c.serialnumber, c.validity, c.issuer
+					From certificates c join (
+						Select *
+						From endpoints join certificateendpoint on endpoints.id=certificateendpoint.endpointid
+						Where ip=$1
+					) AS ED
+					on c.id = ED.certificateid`
+
+	results, err := db.dbsql.Query(queryString, endpoint)
+	if err != nil {
+		return nil, errors.New("GetStateOfEndPoint " + err.Error())
+	}
+
+	var certificateEntries []config.CertificateEntry
+
+	for results.Next() {
+
+		var name, sn, iss string
+		var exp time.Time
+
+		err = results.Scan(&name, &sn, &exp, &iss)
+		if err != nil {
+			log.Println("Parsing Error in GetStateOfEndPoint")
+			continue
+		}
+
+		certificateEntries = append(certificateEntries, config.CertificateEntry{
+			Subject:      name,
+			SerialNumber: sn,
+			Validity:     exp,
+			Issuer:       iss,
+		})
+	}
+
+	return certificateEntries, nil
+}
+
+func (db *Dbmanager) AddKeyEntry(apikey string, apikeyvarname string, apisecret string, apisecretvarname string, dnsprovider string) error {
+	queryString := "Insert into keys values(DEFAULT, $1, $2, $3, $4, $5)"
+	_, err := db.dbsql.Exec(queryString, apikeyvarname, apikey, apisecretvarname, apisecret, dnsprovider)
+	if err != nil {
+		return errors.New("AddKeyEntry " + err.Error())
+	}
+	return nil
+}
+
+func (db *Dbmanager) GetKeyId(apikey string, apisecret string) (int, error) {
+	querString := "Select id From keys Where apikey=$1 and apisecret=$2"
+	row := db.dbsql.QueryRow(querString, apikey, apisecret)
+	var id int
+	err := row.Scan(&id)
+	if err == sql.ErrNoRows {
+		return -1, nil
+	}
+	if err != nil {
+		return -1, errors.New("GetKeyId " + err.Error())
+	}
+	return id, nil
+}
+
+// UpdateCertificateEntry is used to update already existed certificate entry in the certificates table
+func (db *Dbmanager) UpdateCertificateEntry(commonname string, serialnumber string, validity time.Time, rawfile []byte, issuer string) error {
+	queryString := "Update certificates set serialnumber=$1, validity=$2, rawfile=$3, issuer=$4 where commonname=$5"
+	_, err := db.dbsql.Exec(queryString, serialnumber, validity, rawfile, issuer, commonname)
 	if err != nil {
 		return errors.New("UpdateCertificateEntry " + err.Error())
 	}
